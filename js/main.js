11bc477e954a1bddd019e0ca0952e3856a952841
Guanlun Zhao <allen.zhao@goanimate.com>
2016-03-07 21:42:22 +0800

    Initialize the scene

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-var canvas;
-var ctx;
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, 800 / 600, 0.1, 1000);
 
-function init() {
-    canvas = $('#main-canvas').get(0);
-    ctx = canvas.getContext('webgl');
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( 800, 600 );
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function render() {
+    requestAnimationFrame( render );
+    renderer.render( scene, camera );
 }
-
-$(window).load(init);
+render();
