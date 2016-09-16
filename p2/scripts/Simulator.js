530d19e1c69924900f03c2b48ac036592243acb0
Guanlun Zhao <guanlunzhao@gmail.com>
2016-09-16 11:17:25 -0500

    Added P2

---
+++ p2/scripts/Simulator.js
@@ -0,0 +1,200 @@
+module.exports = class Simulator {
+    constructor() {
+        this._objects = [];
+        this._staticPlanes = [];
+        this._forces = [];
+
+        this.elasticity = 0.5;
+        this.frictionCoeff = 0.5;
+        this.airFriction = 0.1;
+    }
+
+    addObject(object) {
+        this._objects.push(object);
+    }
+
+    getObjects() {
+        return this._objects;
+    }
+
+    addStaticPlane(so) {
+        this._staticPlanes.push(so);
+    }
+
+    clearStaticPlanes() {
+        this._staticPlanes = [];
+    }
+
+    simulate(deltaT) {
+        let timeRemaining = deltaT;
+
+        this._objects.forEach(obj => {
+            obj.calculateAcceleration(this._forces);
+
+            this.checkResting(obj);
+        });
+
+        const MAX_ITERATION = 20;
+
+        while (timeRemaining > 0) {
+            let timeSimulated = timeRemaining;
+            this._objects.forEach(obj => {
+                obj.integrate(timeRemaining);
+            });
+
+            const collisions = this.getCollisions();
+
+            let earliestCollisionTime = deltaT;
+            let earliestCollision = null;
+
+            if (collisions.length !== 0) {
+                collisions.forEach(col => {
+                    const obj = col.object;
+                    obj.collisionNormal = col.normal;
+
+                    const collisionTime = col.fraction * deltaT;
+
+                    if (collisionTime < earliestCollisionTime) {
+                        // Get the earliest collision in this timestep
+                        earliestCollisionTime = collisionTime;
+                        earliestCollision = col;
+                    }
+                });
+
+                timeSimulated = earliestCollisionTime;
+
+                this._objects.forEach(obj => {
+                    obj.restoreLastState();
+                    obj.integrate(timeSimulated);
+
+                    if (earliestCollision) {
+                        if (
+                            obj === earliestCollision.object ||
+                            obj === earliestCollision.withObject
+                        ) {
+                            obj.respondToCollision(earliestCollision, this.elasticity, this.frictionCoeff);
+                        }
+                    }
+                });
+            }
+
+            timeRemaining -= timeSimulated;
+
+            this._objects.forEach(obj => {
+                obj.updateLastState();
+            });
+        }
+    }
+
+    getCollisions() {
+        let collisions = [];
+
+        const numObjs = this._objects.length;
+
+        for (let i = 0; i < numObjs; i++) {
+            const obj = this._objects[i];
+            if (!obj.lastState) {
+                continue;
+            }
+
+            const staticPlaneCollisions = this._getStaticPlaneCollisions(obj);
+
+            collisions = collisions.concat(staticPlaneCollisions);
+
+            for (let j = i + 1; j < numObjs; j++) {
+                const obj2 = this._objects[j];
+
+                const objDist = obj.pos.distanceTo(obj2.pos);
+                const lastObjDist = obj.lastState.pos.distanceTo(obj2.lastState.pos);
+
+                const collisionDistance = obj.radius + obj2.radius;
+
+                const collisionNormal = obj2.pos.clone().sub(obj.pos).normalize();
+
+                const lastPos = obj.lastState.pos;
+                const collisionFraction = ((lastObjDist - collisionDistance) / (lastObjDist - objDist));
+
+                if (lastObjDist >= collisionDistance && objDist < collisionDistance) {
+                    collisions.push({
+                        withPlane: false,
+                        object: obj,
+                        withObject: obj2,
+                        normal: collisionNormal,
+                        fraction: collisionFraction,
+                    });
+                }
+            }
+        }
+
+        return collisions;
+    }
+
+    checkResting(obj) {
+        obj.restingAgainst = null;
+
+        // check resting condition
+        this._staticPlanes.forEach(plane => {
+            if (plane.normal.y != 1) {
+                return;
+            }
+
+            const {
+                normal,
+            } = plane;
+
+            const normalVel = obj.vel.clone().multiplyScalar(obj.vel.dot(normal));
+
+            if (normalVel.y > 0.05) {
+                return;
+            }
+
+            const planeDist = obj.pos.y - obj.radius - plane.point.y;
+
+            if (planeDist > 0.05) {
+                return;
+            }
+
+            obj.restingAgainst = plane;
+        });
+    }
+
+    addForce(force) {
+        this._forces.push(force);
+    }
+
+    refreshDispaly() {
+        this._objects.forEach(obj => {
+            obj.updateGraphics();
+        });
+    }
+
+    _getStaticPlaneCollisions(obj) {
+        const collisions = [];
+        this._staticPlanes.forEach(plane => {
+            const {
+                point,
+                normal,
+            } = plane;
+
+            const objPlaneDist = obj.pos.clone().sub(point).dot(normal);
+            const lastObjPlaneDist = obj.lastState.pos.clone().sub(point).dot(normal);
+
+            if (objPlaneDist < obj.radius && lastObjPlaneDist > obj.radius) {
+                const d0 = lastObjPlaneDist - obj.radius;
+                const d1 = objPlaneDist - obj.radius;
+                const collisionFraction = d0 / (d0 - d1);
+
+                collisions.push({
+                    withPlane: true,
+                    normal: normal,
+                    object: obj,
+                    dist: objPlaneDist,
+                    lastDist: lastObjPlaneDist,
+                    fraction: collisionFraction,
+                });
+            }
+        });
+
+        return collisions;
+    }
+}
---
+++ p2/scripts/index.js
@@ -0,0 +1,259 @@
+const Force = require('./Force');
+const Gravity = require('./Gravity');
+const AirFriction = require('./AirFriction');
+const Simulator = require('./Simulator');
+const StaticPlane = require('./StaticPlane');
+const SphereObject = require('./SphereObject');
+const UIControls = require('./UIControls');
+
+const scene = new THREE.Scene();
+
+const props = {
+    stepsPerFrame: 1,
+
+    camera: null,
+    cameraPos: {
+        x: 9,
+        y: 12,
+        z: 15,
+    },
+
+    gravity: null,
+    airFriction: null,
+
+    planes: [],
+    cone: null,
+
+    ball1: null,
+    ball2: null,
+    ball3: null,
+
+    dragging: false,
+    lastMousePos: null,
+}
+
+const simulator = new Simulator();
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+});
+renderer.setSize(720, 500);
+renderer.setClearColor(0x000000, 1);
+document.body.appendChild(renderer.domElement);
+
+renderer.domElement.onwheel = (evt) => {
+    evt.preventDefault(0);
+    const multiplier = 1 + evt.deltaY * 0.001;
+
+    props.cameraPos.x *= multiplier;
+    props.cameraPos.z *= multiplier;
+    props.cameraPos.y *= multiplier;
+
+    props.camera.position.set(props.cameraPos.x, props.cameraPos.y, props.cameraPos.z);
+    props.camera.lookAt(new THREE.Vector3(0, 0, 0));
+}
+
+renderer.domElement.onmousedown = (evt) => {
+    props.dragging = true;
+
+    props.lastMousePos = {
+        x: evt.clientX,
+        y: evt.clientY,
+    };
+}
+
+renderer.domElement.onmousemove = (evt) => {
+    if (!props.dragging) {
+        return;
+    }
+
+    const currMousePos = {
+        x: evt.clientX,
+        y: evt.clientY,
+    };
+
+    const mousePosDiff = {
+        x: currMousePos.x - props.lastMousePos.x,
+        y: currMousePos.y - props.lastMousePos.y,
+    };
+
+    const distXZ = Math.sqrt(props.cameraPos.x * props.cameraPos.x + props.cameraPos.z * props.cameraPos.z);
+    const dist = Math.sqrt(distXZ * distXZ + props.cameraPos.y * props.cameraPos.y);
+
+    const currXZAngle = Math.atan2(props.cameraPos.z, props.cameraPos.x);
+    const newXZAngle = currXZAngle + mousePosDiff.x / 57.3;
+    const currYAngle = Math.atan2(props.cameraPos.y, distXZ);
+    const newYAngle = currYAngle + mousePosDiff.y / 57.3;
+
+    props.cameraPos.x = dist * Math.cos(newYAngle) * Math.cos(newXZAngle);
+    props.cameraPos.z = dist * Math.cos(newYAngle) * Math.sin(newXZAngle);
+    props.cameraPos.y = dist * Math.sin(newYAngle);
+
+    props.camera.position.set(props.cameraPos.x, props.cameraPos.y, props.cameraPos.z);
+    props.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    props.lastMousePos = currMousePos;
+}
+
+renderer.domElement.onmouseup = (evt) => {
+    props.dragging = false;
+}
+
+function initCamera() {
+    props.camera = new THREE.PerspectiveCamera(
+        75,
+        720 / 500,
+        0.1,
+        1000
+    );
+
+    props.camera.position.set(props.cameraPos.x, props.cameraPos.y, props.cameraPos.z);
+    props.camera.lookAt(new THREE.Vector3(0, 0, 0));
+}
+
+function initContainer(shape) {
+    simulator.clearStaticPlanes();
+
+    const containerMaterial = new THREE.MeshPhongMaterial({
+        color: 0x6666ff,
+        specular: 0x000099,
+        transparent: true,
+        opacity: 0.3,
+        side: THREE.DoubleSide,
+    });
+
+    if (shape === 'box') {
+        scene.remove(props.cone);
+
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(0, -10, 0), new THREE.Vector3(0, 1, 0)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(0, 10, 0), new THREE.Vector3(0, -1, 0)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(-10, 0, 0), new THREE.Vector3(1, 0, 0)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(10, 0, 0), new THREE.Vector3(-1, 0, 0)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(0, 0, -10), new THREE.Vector3(0, 0, 1)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(0, 0, 10), new THREE.Vector3(0, 0, -1)));
+
+        const planeGeometry = new THREE.PlaneGeometry(20, 20);
+        const plane1 = new THREE.Mesh(planeGeometry, containerMaterial);
+        plane1.position.set(0, 0, 10);
+        scene.add(plane1);
+
+        const plane2 = new THREE.Mesh(planeGeometry, containerMaterial);
+        plane2.position.set(0, 0, -10);
+        scene.add(plane2);
+
+        const plane3 = new THREE.Mesh(planeGeometry, containerMaterial);
+        plane3.position.set(-10, 0, 0);
+        plane3.rotation.y = -Math.PI / 2;
+        scene.add(plane3);
+
+        const plane4 = new THREE.Mesh(planeGeometry, containerMaterial);
+        plane4.position.set(10, 0, 0);
+        plane4.rotation.y = -Math.PI / 2;
+        scene.add(plane4);
+
+        props.planes = [plane1, plane2, plane3, plane4];
+
+    } else if (shape === 'pyramid') {
+        props.planes.forEach(p => scene.remove(p));
+
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(0, -10, 0), new THREE.Vector3(0, 1, 0)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(-10, -10, 0), new THREE.Vector3(0.866, -0.5, 0)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(10, -10, 0), new THREE.Vector3(-0.866, -0.5, 0)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(0, -10, -10), new THREE.Vector3(0, -0.5, 0.866)));
+        simulator.addStaticPlane(new StaticPlane(new THREE.Vector3(0, -10, 10), new THREE.Vector3(0, -0.5, -0.866)));
+
+        const coneGeometry = new THREE.ConeGeometry(
+            7.07 * 2, 10 * 2, 4
+        );
+
+        props.cone = new THREE.Mesh(coneGeometry, containerMaterial);
+        props.cone.position.set(0, 0, 0);
+        props.cone.rotation.y = -Math.PI / 4;
+        scene.add(props.cone);
+    }
+}
+
+function initForces() {
+    props.gravity = new Gravity();
+    props.airFriction = new AirFriction();
+}
+
+function initObjects() {
+    const particles = new THREE.Geometry();
+
+    for (let i = 0; i < 5000; i++) {
+        const x = Math.random() * 400 - 200;
+        const y = Math.random() * 400 - 200;
+        const z = Math.random() * 400 - 200;
+
+        const particle = new THREE.Vector3(x, y, z);
+        particles.vertices.push(particle);
+    }
+
+    const particleMaterial = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size: 5,
+        blending: THREE.AdditiveBlending,
+        transparent: true,
+        opacity: 0.5,
+    });
+
+    const particleSystem = new THREE.Points(particles, particleMaterial);
+
+    scene.add(particleSystem);
+}
+
+function initLight() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(0, 30, 30);
+    scene.add(directionalLight);
+}
+
+function initControls() {
+    const controls = new UIControls();
+
+    // controls.addListener('reset-button-clicked', throwIntoTheAir);
+
+    controls.addListener('container-shape-changed', (shape) => {
+        initContainer(shape);
+    });
+
+    controls.addListener('step-size-changed', val => {
+        props.stepsPerFrame = val;
+    });
+
+    controls.addListener('elasticity-changed', val => {
+        simulator.elasticity = val;
+    });
+
+    controls.addListener('friction-coeff-changed', val => {
+        simulator.frictionCoeff = val;
+    });
+
+    controls.addListener('air-friction-changed', val => {
+        props.airFriction.setCoefficient(val);
+    });
+}
+
+initCamera();
+initForces();
+initObjects();
+initContainer('box');
+initLight();
+initControls();
+
+function simulate() {
+    for (let i = 0; i < props.stepsPerFrame; i++) {
+        simulator.simulate(0.01 / props.stepsPerFrame);
+    }
+
+    simulator.refreshDispaly();
+
+    renderer.render(scene, props.camera);
+
+    requestAnimationFrame(simulate);
+}
+simulate();
